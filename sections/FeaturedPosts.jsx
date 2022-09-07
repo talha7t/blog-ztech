f5070ff7ded585d6308c4fd6432b8c5ab32ebce8
talha7t <jtalha620@gmail.com>
2022-09-06 19:59:24 -0700

    implemented categories

---
+++ sections/FeaturedPosts.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import Carousel from "react-multi-carousel";
+import { getFeaturedPosts } from "../services";
+import { FeaturedPostCard } from "../components";
+
+import "react-multi-carousel/lib/styles.css";
+
+// responsive break points for carousel
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+function FeaturedPosts() {
+  const [featuredPosts, setFeaturedPosts] = useState([]);
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    getFeaturedPosts().then((result) => {
+      setFeaturedPosts(result);
+      setDataLoaded(true);
+    });
+  }, []);
+
+  const customLeftArrow = (
+    <div className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 mx-auto text-white w-full"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg>
+    </div>
+  );
+
+  const customRightArrow = (
+    <div className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-pink-600 rounded-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 mx-auto text-white w-full"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg>
+    </div>
+  );
+
+  return (
+    <div className="mb-4">
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        customLeftArrow={customLeftArrow}
+        customRightArrow={customRightArrow}
+        itemClass="px-4"
+      >
+        {dataLoaded &&
+          featuredPosts.map((post) => (
+            <FeaturedPostCard post={post} key={post.id} />
+          ))}
+      </Carousel>
+    </div>
+  );
+}
+
+export default FeaturedPosts;
--- services/index.js
+++ services/index.js
@@ -91,6 +91,7 @@ export const getRecentPosts = async () => {
   const result = await request(graphqlAPI, query);
   return result.posts;
 };
+
 export const getSimilarPosts = async (slug, categories) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
@@ -154,4 +155,108 @@ export const getComments = async (slug) => {
 
   const result = await request(graphqlAPI, query, { slug });
   return result.comments;
+};
+
+export const getFeaturedPosts = async () => {
+  const query = gql`
+    query GetFeaturedPosts {
+      posts(where: { featuredPost: true }) {
+        id
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        author {
+          name
+          bio
+          avatar {
+            url
+          }
+        }
+        createdAt
+        slug
+        content {
+          raw
+        }
+        categories {
+          name
+          slug
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
+
+export const getAdjacentPosts = async (createdAt, slug) => {
+  const query = gql`
+    query GetAdjacentPosts($createdAt: DateTime!, $slug: String!) {
+      next: posts(
+        first: 1
+        orderBy: createdAt_ASC
+        where: { slug_not: $slug, AND: { createdAt_gte: $createdAt } }
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+      previous: posts(
+        first: 1
+        orderBy: createdAt_DESC
+        where: { slug_not: $slug, AND: { createdAt_lte: $createdAt } }
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug, createdAt });
+  return { next: result.next[0], previous: result.previous[0] };
+};
+
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              avatar {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+  return result.postsConnection.edges;
 };
